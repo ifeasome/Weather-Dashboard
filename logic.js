8f62e93f0dd9e6868a0a8885b991c5346d5c96a1
Somelina Obiechina <ifeasome@gmail.com>
2020-11-08 00:09:28 -0600

    stylesheet linked
    Included div elements and break tags

--- logic.js
+++ logic.js
@@ -1,24 +1,95 @@
 $(document).ready(function () {
-    let inputText = $("#city-search").val();
-    let APIkey = ac2619886a7a1a9a4582c78a9fb57698
+    
+
+    //let APIkey = ac2619886a7a1a9a4582c78a9fb57698;
+    let cityStore = [];
+
+    function cityList () {
+       $(".search-list").empty();
+
+        let cityCntr = 0; 
+        console.log(JSON.parse(localStorage.getItem("key")));
+        while (cityCntr < JSON.parse(localStorage.getItem("key")).length) {
+            let ulEL = $("<ul>");
+            ulEL.text(JSON.parse(localStorage.getItem("key"))[cityCntr]);
+            $(".search-list").append(ulEL); cityCntr++;
+        }
+
+     
+        // cityStore[0].prependTo create new list element under search box on the HTML 
+        // 0. let newVar2 = 0; 
+        // 0.5 while newVar 2 < JSON.parse(localStorage.getItem("key")).length; 
+        // 1. CREATE HTML : let a variable = $("<li>")
+        // 2.  variable.text (JSON.parse(localStorage.getItem("key"))[newVar2])
+        // 3.. $(".search-list").append (variable); newVar2++;
+
+
+    }
 
     $(".btn").on("click", function(){
-        queryURL = api.openweathermap.org/data/2.5/weather?q={inputText} + APIkey;
-    })
+        
+        let inputText = $("#city-search").val();
+
+        let urlOne = "https://api.openweathermap.org/data/2.5/weather?q=" + inputText + "&appid=ac2619886a7a1a9a4582c78a9fb57698"; 
+
+    
+        cityStore.push(inputText);
+
+         
+        localStorage.setItem ("key", JSON.stringify(cityStore));
+
+       cityList();
+
+        
+        
+        console.log(inputText);
+        
+    
+        $.ajax({
+            url: urlOne,
+            method: "GET"
+        })
+           .then(function(response){
+               let results = response;
+               console.log(results);
+              
+               let lonEL = response.coord.lon; 
+
+               let latEL = response.coord.lat;
+
+               let urlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEL + "&lon=" + lonEL + "&exclude={part}&appid=ac2619886a7a1a9a4582c78a9fb57698";
+
+               $.ajax({
+        
+                   url: urlTwo,
+                   method: "GET"
+               })
+
+                 .then(function(response) {
+                     console.log(response);
+                 })
+
+           let cityDisplay = $("#city-search").val();
+           console.log(cityDisplay, inputText);
+           // $("p").append("city-search").val();
+           //$(cityDisplay).text = $("#city-search").val();
+           // $(dateDisplay).text(moment().format("MMMM, Do, YYYY"));
+          //  $(tempDisplay).text = response.current.temp;
+           // $(humidityDisplay).text = response.current.humidity;
+           // $(windspeedDisplay).text = response.current.wind_speed;
+           // $(uvDisplay).text = response.current.uvi;
+           
+           });
+
+           
+
+          
+           
+           })
+              });
+    
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-});
 
