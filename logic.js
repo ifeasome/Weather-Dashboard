5f304badc06be28e128db48af9c7cd3bb5a4ef03
Somelina Obiechina <ifeasome@gmail.com>
2020-11-08 03:46:04 -0600

    working on display weatherboard logic

--- logic.js
+++ logic.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
 
                let latEL = response.coord.lat;
 
-               let urlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEL + "&lon=" + lonEL + "&exclude={part}&appid=ac2619886a7a1a9a4582c78a9fb57698";
+               let urlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEL + "&lon=" + lonEL + "&units=imperial&exclude={part}&appid=ac2619886a7a1a9a4582c78a9fb57698";
              
                // second ajax call relying on first ajax call results to retrive weather info
                $.ajax({
@@ -63,21 +63,33 @@ $(document).ready(function () {
                })
 
                  .then(function(response) {
+                     console.log(response);
                      
                  })
 
+                 // weather display board
+                 // temp display 
+
                  let kTemp = response.main.temp; 
                  let fTemp = (kTemp - 273.15) * 1.80 + 32;
+                
 
                  let tempEL = $(".temp");
 
                  tempEL.text("Temperature: " + Math.floor(fTemp));
-
+                
+                 // city display
                  let cityEL = $(".city");
                  let dateEL = moment().format('L');
-                 console.log(dateEL);
                  cityDisp = inputText + " " + "(" + dateEL + ")";
                  cityEL.text(cityDisp);
+
+                 // wind display 
+                 let windDisp = $(".wind"); 
+                 let windEL = response.main.wind_speed;
+
+                 console.log(windEL);
+
                  
 
                  
